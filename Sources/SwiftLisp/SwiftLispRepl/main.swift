e255eb908e27a4e4ee4491e9a0ba615c15109872
merispizza <meris@dev.local>
2019-01-03 21:18:44 +0100

    add ability to read file from the 'repl' executable xD

--- Sources/SwiftLisp/SwiftLispRepl/main.swift
+++ Sources/SwiftLisp/SwiftLispRepl/main.swift
@@ -1,14 +1,24 @@
 @testable import SwiftLispLib
 import Foundation
-var input: String = ""
-while let line = readLine() { input += line }
-let lexOutput = lex(input: input)
-let exprs: [Expr] = read(input: lexOutput)
-let result: Result<Expr> = eval(exprs)
-switch result {
-  case .error(let e):
-    print(e)
+let arguments = CommandLine.arguments
+if (arguments.count == 2) {
+  let filePath = arguments[1]
+  let fileContent = try? String(contentsOfFile: filePath)
+  if let input = fileContent {
+    let lexOutput = lex(input: input)
+    let exprs: [Expr] = read(input: lexOutput)
+    let result: Result<Expr> = eval(exprs)
+    switch result {
+    case .error(let e):
+      print(e)
+      exit(1)
+    default:
+      exit(0)
+    }
+  } else {
+    print("Cannot find file: \(filePath)")
     exit(1)
-  default:
-    exit(0)
+  }
+} else {
+  print("1 argument, file path pls")
 }
