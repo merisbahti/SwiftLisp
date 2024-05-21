e19c70f0d4ac1c6533a41bdac2c5560af3ce75f0
meris <meris.bahti@gmail.com>
2024-05-21 20:29:26 +0200

    upgrade

--- Package.swift
+++ Package.swift
@@ -1,34 +1,33 @@
-// swift-tools-version:4.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftLisp",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftLispLib",
-            targets: ["SwiftLispLib"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/davedufresne/SwiftParsec", from: "3.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "SwiftLispLib",
-            dependencies: ["SwiftParsec"],
-            path: "Sources/SwiftLisp/SwiftLispLib"
-            ),
-        .target(
-            name: "SwiftLispRepl",
-            dependencies: ["SwiftLispLib"],
-            path: "Sources/SwiftLisp/SwiftLispRepl"
-            ),
-        .testTarget(
-            name: "SwiftLispTests",
-            dependencies: ["SwiftLispLib"])
-    ]
+  name: "SwiftLisp",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "SwiftLispLib",
+      targets: ["SwiftLispLib"]),
+    .executable(name: "SwiftLispRepl", targets: ["SwiftLispRepl"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/davedufresne/SwiftParsec", from: "4.0.0")
+  ],
+  targets: [
+    .target(
+      name: "SwiftLispLib",
+      dependencies: ["SwiftParsec"],
+      path: "Sources/SwiftLisp/SwiftLispLib"
+    ),
+    .executableTarget(
+      name: "SwiftLispRepl",
+      dependencies: ["SwiftLispLib"],
+      path: "Sources/SwiftLisp/SwiftLispRepl"
+    ),
+    .testTarget(
+      name: "SwiftLispTests",
+      dependencies: ["SwiftLispLib"]),
+  ]
 )
