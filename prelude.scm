93ac0ef58bd9956f57b91ea71b20edc96027ada1
meris <meris.bahti@gmail.com>
2024-05-28 21:28:22 +0200

    correct 2.21

--- prelude.scm
+++ prelude.scm
@@ -9,6 +9,14 @@
     (cond
       ((eq x '()) true)
       (true false))))
+(define nil '())
+
+(define (map proc items)
+  (cond
+    ((null? items) nil)
+    (else
+      (cons (proc (car items))
+        (map proc (cdr items))))))
 
 (def filter
   (fn (pred xs)
