1e0a61b4af6b3b53fec8465c1b7b12d61f5852fa
meris <meris.bahti@gmail.com>
2024-05-30 20:18:35 +0200

    2.22

--- sols/2.22.scm
+++ sols/2.22.scm
@@ -8,4 +8,17 @@
           answer))))
   (iter items nil))
 
+(print "it's because cons adds to the front of the list, and we're processing the first item first.")
+(assert (square-list '(1 2 3 4)) '(16 9 4 1))
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+      answer
+      (iter (cdr things)
+        (cons answer
+          (square (car things))))))
+  (iter items nil))
+
+(print "it still puts things in the wrong order, but now the list is in the 'head' position")
 (assert (square-list '(1 2 3 4)) '(16 9 4 1))
