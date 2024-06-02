224244ab1887da03af0f465b6890676e4f8b47c3
meris <meris.bahti@gmail.com>
2024-06-02 17:46:18 +0200

    2.33

--- prelude.scm
+++ prelude.scm
@@ -22,12 +22,12 @@
 (define (newline) (print ""))
 (define display print)
 
-(def filter
-  (fn (pred xs)
-    (cond
-      ((eq xs '()) '())
-      ((pred (car xs)) (cons (car xs) (filter pred (cdr xs))))
-      (true (filter pred (cdr xs))))))
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+    ((predicate (car sequence))
+      (cons (car sequence)
+        (filter predicate (cdr sequence))))
+    (else (filter predicate (cdr sequence)))))
 
 (define = eq)
 
@@ -58,3 +58,20 @@
         ((null? x) acc)
         (true (reverse-iter (cdr x) (cons (car x) acc))))))
   (reverse-iter x '()))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+      (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+    nil
+    (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+    ((not (pair? tree)) (list tree))
+    (else (append (enumerate-tree (car tree))
+           (enumerate-tree (cdr tree))))))
