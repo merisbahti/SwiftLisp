072689cdaa5c662cd4a0a21e981556d0c9c3acbf
meris <meris.bahti@gmail.com>
2024-06-03 20:23:53 +0200

    add nicer error message

--- Sources/SwiftLisp/SwiftLispLib/Eval.swift
+++ Sources/SwiftLisp/SwiftLispLib/Eval.swift
@@ -77,7 +77,7 @@ public func eval(_ expr: Expr, _ env: Env) -> EvalResult {
   case .pair((let car, let cdr)):
     guard case .success((.fun(let carEvaled), _)) = eval(car, env) else {
       return makeEvalError(
-        "car of pair is not a function, found: \(car) in pair \(expr)")
+        "\(car) is not a function (in pair \(expr))")
     }
     let pairs = collectPairs(cdr)
     guard case .success(let args) = pairs else {
